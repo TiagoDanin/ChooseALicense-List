1097f29a75c382afcd5a0b6f511dfc62c694246e
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-04-15 20:37:04 -0700

    Fix mutating license list

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var list = require('./licenses.json')
-list.license = list
+var licenses = require('./licenses.json')
+var list = Object.assign({}, licenses)
+list.license = licenses
+list.list = licenses
 list.find = (id) => {
 	if (typeof id !== 'string') {
 		throw new TypeError('Expected a string');
 	}
-	for (let license in list) {
+	for (let license in licenses) {
 		if (license.toLocaleLowerCase() === id.toLocaleLowerCase()) {
-			return list[license]
+			return licenses[license]
 		}
 	}
 	return false
--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 const choosealicenseList = require('.')
 const assert = require('assert')
-
+const licenses = require('./licenses.json')
 
 assert.equal(choosealicenseList.find('mit').id, 'MIT')
 assert.equal(choosealicenseList.MIT.body.length > 0, true)
 assert.equal(choosealicenseList.hasOwnProperty('data'), true)
 assert.equal(choosealicenseList.MIT.permissions.length == 4, true)
+assert.equal(choosealicenseList.find('data'), false)
+assert.notStrictEqual(choosealicenseList.license, choosealicenseList)
+assert.strictEqual(choosealicenseList.list, licenses)
 
 console.log('Done!')
